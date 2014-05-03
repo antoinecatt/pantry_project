b331317ccca1d4e741e8e67e65be06e4a6ce5e9c
antoinecatt <antoinecatt@gmail.com>
2014-05-03 10:10:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pantry::Application.config.secret_key_base = '9b42eb1aadcaef4744784a1ba2f8412ddaaef5cd2ece71b71727ed627c55a1226dd18866f9d453fdef201800df6c1b9fc4ab6fe6a1d908e1e152c1d1df626aea'
