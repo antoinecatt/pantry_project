af583a56f81de24ab5e91a8783735a08bec61be0
antoinecatt <antoinecatt@gmail.com>
2014-05-05 09:07:24 -0700

    created index view for ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,13 +4,24 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.new
   end
 
+  def index
+    @ingredients = Ingredient.all
+  end
+
   def create
     ingredient = Ingredient.create(ingredient_params)
+    ingredient.save
     redirect_to ingredient
   end
 
   def show
      @ingredient = Ingredient.find(params[:id])
+  end
+
+  def destroy
+    ingredient = Ingredient.find(params[:id])
+    ingredient.delete
+    redirect_to ingredients_path
   end
 
   private
