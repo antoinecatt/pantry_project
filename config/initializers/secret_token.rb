b909c3edab81893dc6f7fc3db6124f364c8525c5
antoinecatt <antoinecatt@gmail.com>
2014-05-05 10:58:45 -0700

    generate

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Pantry::Application.config.secret_key_base = '9b42eb1aadcaef4744784a1ba2f8412ddaaef5cd2ece71b71727ed627c55a1226dd18866f9d453fdef201800df6c1b9fc4ab6fe6a1d908e1e152c1d1df626aea'
+Pantry::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
