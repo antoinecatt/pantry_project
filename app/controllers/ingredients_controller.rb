0a0e6b569ec8f154f6a515d619ad8fca247b009f
antoinecatt <antoinecatt@gmail.com>
2014-05-08 16:18:08 -0700

    added bootstrap, added devise views, fixed list page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,7 +3,6 @@ class IngredientsController < ApplicationController
   
   def new
     @ingredient = Ingredient.new
-   
   end
 
   def index
@@ -25,6 +24,7 @@ class IngredientsController < ApplicationController
      @parse = JSON.parse(response.body)
      size = @parse['matches'].size
      @recipe = @parse['matches'][rand(size)]
+     
   end
 
   def details
@@ -33,6 +33,15 @@ class IngredientsController < ApplicationController
     #binding.pry
     @detail = JSON.parse(response.body)
     
+  end
+
+  def list
+    @ingredient_name = params[:name]
+    # @ingredient = Ingredient.find(params[:id])
+     # response = Typhoeus.get("http://api.yummly.com/v1/api/recipes?_app_id=ffdd32f5&_app_key=dd268f25fe5a30f7c1821def3698bf46&allowedIngredient[]=#{@ingredient.name}")
+     response = Typhoeus.get("http://api.yummly.com/v1/api/recipes?_app_id=ffdd32f5&_app_key=dd268f25fe5a30f7c1821def3698bf46&allowedIngredient[]=#{@ingredient_name}")
+    
+     @recipes = JSON.parse(response.body)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Pantry::Application.routes.draw do
 devise_for :users
+
+
 root to: "ingredients#index"
 #root 'ingredients#new'
 resources :ingredients
 get '/details/:id' => 'ingredients#details'
+get '/list/:name' => 'ingredients#list'
+
 end
